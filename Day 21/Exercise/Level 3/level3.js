96ffbb3049d772d6af889536d086fcc520b055c3
Kanak4506 <kanaksehrawat456@gmail.com>
2024-07-05 16:26:19 +0530

    updated

---
+++ Day 21/Exercise/Level 3/level3.js
@@ -0,0 +1,6 @@
+var title = document.getElementsByTagName('h1');
+// title.style.fontFamily = 'cursive';
+// let updated = title.innerText.replace(/2020/, '2021');
+console.log("hello")
+
+title.style.color = "red";
